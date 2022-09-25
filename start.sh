404442de82105f37ae84b0b7e87d728d2d1b7d74
naybiblu <iamsuperv15@gmail.com>
2022-09-25 10:57:51 +0000

    Testing deployment

--- start.sh
+++ start.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-docker run -it --rm --entrypoint sh "$@" -c "
-
-          [ -x /usr/sbin/useradd ] && useradd -m -u $(id -u) u1 -s /bin/sh || adduser -D -u $(id -u) u1 -s /bin/sh;
-
-          exec su - u1"
-
-ssh-keygen -t rsa b 4096 -C "iamsuperv15@gmail.com"
-
 echo "------------------
 Adding every file to the stage...
 ------------------"
