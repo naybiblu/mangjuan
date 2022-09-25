858decd54e326f882632e50113eed7a4442dce92
naybiblu <iamsuperv15@gmail.com>
2022-09-25 17:32:45 +0000

    Update

--- src/events/error.js
+++ src/events/error.js
@@ -1,11 +1,35 @@
+const chalk = require("chalk");
+
 module.exports = {
     name: "error",
     type: "public",
     async execute (client) {
     
         client.on("error", (err) => {
+            console.log(chalk.bold("[Discord]: ") + chalk.red.bold("Catched error from the API:"));
             console.error(err);
             });
     
+        process.on('uncaughtException', (err, origin) => {
+
+  console.log(chalk.bold("[NodeJs]: ") + chalk.red.bold(`Malfunctioned from ${origin}:`));
+            console.error(err);
+
+})
+
+process.on("unhandledRejection", (reason, p) => {
+
+  console.log(chalk.bold("[NodeJs]: ") + chalk.red.bold(`Promise ${p} unhandled due to:`));
+            console.error(reason);
+
+})
+
+process.on("uncaughtExceptionMonitor", (err, origin) => {
+
+  console.log(chalk.bold("[NodeJs]: ") + chalk.red.bold(`Malfunctioned from ${origin}:`));
+
+            console.error(err);
+
+});
     }
 }
