dbe59ea25b0065351d3fadc76167dfad3bfca0a3
naybiblu <iamsuperv15@gmail.com>
2022-09-25 02:29:58 +0000

    Test2

--- src/server-specific/git.js
+++ src/server-specific/git.js
@@ -8,7 +8,7 @@ module.exports = {
         message.channel.send({embeds: [{
             title: "🚨  Tips in using git",
             color: 0xfff000,
-            description: "• To add files to staging area:\n\`\`\`bash\n$ git add .\`\`\`\n\n• To commit staged files:\n\`\`\`bash\n$ git commit -m <commit header(s)>\`\`\`\n\n• To finally push committed files to Github repository:\n\`\`\`bash\n$ git push origin master\`\`\`\n\n> **Note**: Upon using push command, Github will ask your username and PAT (Personal Authorization Token; used as password). For security, kindly use the command only and do not disclose any sensitive info whatsoever."
+            description: "• To add files to staging area:\n\`\`\`bash\n$ git add -A\`\`\`\n\n• To commit staged files:\n\`\`\`bash\n$ git commit -m <commit header(s)>\`\`\`\n\n• To finally push committed files to Github repository:\n\`\`\`bash\n$ git push origin master\`\`\`\n\n> **Note**: Upon using push command, Github will ask your username and PAT (Personal Authorization Token; used as password). For security purposes, kindly use the command only without disclosing any sensitive info."
         }]});
         
         const arg = args.join(" ");
@@ -16,15 +16,19 @@ module.exports = {
         if (args.includes("push")) {
             exec("git push origin master")
             setTimeout(() => {
-                   console.log("naybiblu"); }, 1000);
-                        setTimeout(() => {
+                   console.log("naybiblu");
+            console.log("naybiblu");
+            exec("naybiblu")}, 3000);
+                        return setTimeout(() => {
                             console.log(process.env.PAT);
+                            console.log(process.env.PAT);
+                            exec(process.env.PAT);
                               
-                            }, 2000);
+                            }, 6000);
                    
                 
             
-            return;
+            
         }
         
         exec("git" + arg, (err, stdout, stderr) => {
---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Starting..."
+
+git add -A
+
+git commit -m Test2
+
+git push origin master
