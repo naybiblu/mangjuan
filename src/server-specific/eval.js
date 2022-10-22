0c75c1066bb7a98206e6f69a47d82971f5b00fc5
mharcnyvhielguillermo@iskolarngbayan.pup.edu.ph <mharcnyvhielguillermo@iskolarngbayan.pup.edu.ph>
2022-10-22 11:52:51 +0800

    Optimizing eval command

--- src/server-specific/eval.js
+++ src/server-specific/eval.js
@@ -3,7 +3,13 @@ module.exports = {
     aliases: ["e"],
     servers: [0, 1],
     async execute (client, message, args) {
-    const arg = args.join(" ");
-    eval(arg);
+      const arg = args.join(" ");
+      
+      if (!arg.startsWith("\`\`\`js") && !arg.endsWith("\`\`\`")) return message.channel.send({embeds: [{
+          description: "⚠️ **|** Evaluate code within code block format please! JavaScript is only accepted to be evaluated.",
+          color: 0xff0000
+      }]});
+      
+      eval(arg.replace("\`\`\`js", "").replace("\`\`\`", ""));
     }
 }
