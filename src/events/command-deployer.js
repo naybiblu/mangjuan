d6a15c0177fca9b7415a6a80c44d605376f468d5
mharcnyvhielguillermo@iskolarngbayan.pup.edu.ph <mharcnyvhielguillermo@iskolarngbayan.pup.edu.ph>
2022-11-03 00:24:54 +0800

    Debugging v8

--- src/events/command-deployer.js
+++ src/events/command-deployer.js
@@ -1,4 +1,4 @@
-const { REST, Routes } = require("discord.js");
+//const { REST, Routes } = require("discord.js");
 const fs = require("fs");
 const chalk = require("chalk");
 require("dotenv").config();
