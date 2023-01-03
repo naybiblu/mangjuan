0baba1eac6b2309cf8c39b49b96b65a11487caf7
mharcnyvhielguillermo@iskolarngbayan.pup.edu.ph <mharcnyvhielguillermo@iskolarngbayan.pup.edu.ph>
2023-01-03 12:51:36 +0800

    Fixed role command v2

--- src/events/command-deployer.js
+++ src/events/command-deployer.js
@@ -16,7 +16,7 @@ module.exports = {
           
           const slash = require(`./../commands/${com}`);
           
-          commands.push(slash.data.toJSON());
+          commands.push(slash.data);
           
       }
        
